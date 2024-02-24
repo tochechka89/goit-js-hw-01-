651a75899042774990cd87e25c3360734ae681be
tochechka89 <rabota20ton@gmail.com>
2024-02-24 18:48:56 +0200

    2

--- js/task-1.js
+++ js/task-1.js
@@ -1,12 +1,7 @@
 
-function makeTransaction (quantity, totalPrice) {
-    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+function makeTransaction (quantity, pricePerDroid) {
+    return `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`;
 }
-
-let quantity;
-let pricePerDroid;
-
-let totalPrice = quantity * pricePerDroid;
 
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
 console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
